97151540ac1109d856ec50a2341be6ca21a99771
Brian Archibald <brianallenarchibald@gmail.com>
2021-07-03 14:01:03 -0500

    fib with dp

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -6,3 +6,14 @@ def fib(n):
     return fib(n-1) + fib(n-2)
 
 print(fib(7))
+
+#  using dp and memoization
+
+def fib(n, memo={}):
+    if n in memo: return memo[n]
+    if n <= 2: return 1
+    
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n]
+
+print(fib(60))
